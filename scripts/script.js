9814446351f8ef44971128e6209a207930d0f99c
Aaron Erb <erbnlegend@gmail.com>
2022-10-05 22:45:26 -0500

    Clean up JS. Change 'For Loop' to iterate 5 Rounds

---
+++ scripts/script.js
@@ -0,0 +1,96 @@
+// Rock Paper Scissors Game //
+
+/*
+Need to get input (Rock, Paper, Scissors) from AI
+-Needs to be a random choice
+-Store option
+
+Need to get input (Rock, Paper, Scissors) from player
+-Store option
+-Needs to be a typed option
+--Has to be an equivalent of rock, paper, scissors
+-needs to accept any type of case (case insensitive)
+
+Need to compare the options selected together
+
+Need to know order of power (which option beats what?)
+
+Output message to user on the results
+
+//
+
+Rock > Scissors
+Rock < Paper
+
+Paper > Rock
+Paper < Scissors
+
+Scissors > Paper
+Scissors < Rock
+
+//
+
+
+*/
+
+// Player Selection
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+async function initGameSelection() {
+    console.log("Started");
+    let playerWins = 0;
+    let compWins = 0;
+    let computer = "Rock";
+    let logged = 0;
+// Get player selection and check to ensure it is acceptable
+    for(let i = 0; i < 5; i++) {
+    initSelection();
+    }
+    function initSelection() {
+        let playerPrompt = prompt("Enter Rock, Paper or Scissors!");
+        if(playerPrompt == "Rock" || playerPrompt == "Paper" || playerPrompt == "Scissors") {
+            let player = playerPrompt;
+            // Start Game comparaisons (Who wins??)
+            function compareSelections(player, computer) {
+                //Check Player Wins
+                if (player === "Rock" && computer === "Scissors") {
+                    playerWins++;
+                    console.log(`Player Wins! Player:${playerWins} Computer:${compWins}`);
+                } else if(player === "Paper" && computer === "Rock") {
+                    playerWins++;
+                    console.log(`Player Wins! Player:${playerWins} Computer:${compWins}`);
+                } else if(player === "Scissors" && computer === "Paper") {
+                    playerWins++;
+                    console.log(`Player Wins! Player:${playerWins} Computer:${compWins}`);
+                // Computer Wins
+                } else if(player === "Rock" && computer === "Paper") {
+                    compWins++;
+                    console.log(`Computer Wins! Player:${playerWins} Computer:${compWins}`);
+                } else if(player ==="Paper" && computer === "Scissors") {
+                    compWins++;
+                    console.log(`Computer Wins! Player:${playerWins} Computer:${compWins}`);
+                } else if(player === "Scissors" && computer === "Rock") {
+                    compWins++;
+                    console.log(`Computer Wins! Player:${playerWins} Computer:${compWins}`);
+                // TIE!
+                } else if (player === computer) {
+                    console.log(`No Contest! Try again! Player:${playerWins} Computer:${compWins}`);
+                }
+            }    
+            compareSelections(player, computer)
+        } else {
+            console.log("Did you not listen?? Re-enter option please!");
+            initSelection();
+        }
+    }
+}
+
+initGameSelection()
+
+});
+
+
+
+
+
+
